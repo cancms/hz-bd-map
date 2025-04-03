5cd94e3cd669c6b9665ad17da40994567470b225
cancms <cancms@163.com>
2025-04-04 01:23:03 +0800

    upload project files

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,54 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path";
+import {resolve} from "path";
 import Components from 'unplugin-vue-components/vite';
 import AutoImport from 'unplugin-auto-import/vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
 import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/styles/var.scss";`
-      }
-    }
-  },
-  plugins: [vue(),
-  Components({
-    resolvers: [AntDesignVueResolver()],
-  }),
-  AutoImport({
-    resolvers: [AntDesignVueResolver()],
-  }),
-  viteCompression({
-    verbose: true, // 默认即可
-    disable: false, //开启压缩(不禁用)，默认即可
-    deleteOriginFile: false, //删除源文件
-    threshold: 10240, //压缩前最小文件大小
-    algorithm: 'gzip', //压缩算法
-    ext: '.gz', //文件类型
-  })
-  ],
-  // server: {
-  //   // 'https://c.m.163.com/ug/api/wuhan/app/data/list-total'
-  //   proxy: {
-  //     '/163': {
-  //       target: 'https://c.m.163.com/',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/163/, '')
-  //     }
-  //   }
-  // },
+
+    base: '/hz-bd-map/',  // 远程服务器配置 ， 设置公共基础路径
+    // base: '/', // 本地开发
+
+
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "./src"),
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/styles/var.scss";`
+            }
+        }
+    },
+    plugins: [vue(),
+        Components({
+            resolvers: [AntDesignVueResolver()],
+        }),
+        AutoImport({
+            resolvers: [AntDesignVueResolver()],
+        }),
+        viteCompression({
+            verbose: true, // 默认即可
+            disable: false, //开启压缩(不禁用)，默认即可
+            deleteOriginFile: false, //删除源文件
+            threshold: 10240, //压缩前最小文件大小
+            algorithm: 'gzip', //压缩算法
+            ext: '.gz', //文件类型
+        })
+    ],
+    // server: {
+    //   // 'https://c.m.163.com/ug/api/wuhan/app/data/list-total'
+    //   proxy: {
+    //     '/163': {
+    //       target: 'https://c.m.163.com/',
+    //       changeOrigin: true,
+    //       rewrite: (path) => path.replace(/^\/163/, '')
+    //     }
+    //   }
+    // },
 })
